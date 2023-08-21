6f978d70a0c099b31282e81286e3861a143f31e0
sfs <2564642871@qq.com>
2023-08-21 09:12:53 +0800

    first

---
+++ src/views/Protected.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Greeting, {{ username }}</h1>
+        <button @click="logout">Logout</button>
+        <router-link :to="{ name: 'invoices' }">
+            <button>Invoices</button>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: window.user
+        }
+    },
+    methods: {
+        logout() {
+            window.user = null
+            this.$router.push({ name: 'Home', query: { logout: null } })
+        }
+    }
+
+}
+</script>
